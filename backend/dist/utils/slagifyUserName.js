77f1e8e94d867b5565fc69b9ce04570f2b38e3ce
PriteeAr0te <arotepritee111@gmail.com>
2025-05-25 11:32:48 +0530

    build folder deleted

--- backend/dist/utils/slagifyUserName.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.slugifyUserName = slugifyUserName;
-function slugifyUserName(name) {
-    return name.trim().toLowerCase().replace(/\s+/g, "-");
-}
-//# sourceMappingURL=slagifyUserName.js.map
